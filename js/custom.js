34899c9b46f1faeff0fe052f77e6871bd7360850
Huzefa Malvi <huzefa.malvi@weboniselab.com>
2023-02-02 12:02:55 +0530

    update files

--- js/custom.js
+++ js/custom.js
@@ -82,4 +82,32 @@ jQuery('document').ready(function(){
       autoplayHoverPause : true,
       mouseDrag: true
     });
+
+    var owl = jQuery('#testimonial .owl-carousel');
+      owl.owlCarousel({
+      margin:30,
+      nav: true,
+      autoplay : false,
+      lazyLoad: true,
+      autoplayTimeout: 3000,
+      loop: false,
+      dots:false,
+      navText : ['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
+      responsive: {
+        0: {
+          items: 1
+        },
+        576: {
+          items: 2
+        },
+        768: {
+          items: 3
+        },
+        1000: {
+          items: 3
+        }
+      },
+      autoplayHoverPause : true,
+      mouseDrag: true
+    });
 });
